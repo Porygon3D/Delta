18bbb02a8c12228963363421e8dd86d50b547d53
Holden Thompson <holden.j.thompson@outlook.com>
2025-09-20 18:30:08 -0400

    Added Some Comments

--- Game1.cs
+++ Game1.cs
@@ -3,7 +3,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 
-
 namespace Delta;
 //Written By Holden Thompson - SandboxSoftware 
 public class Game1 : Game
@@ -21,23 +20,32 @@ public class Game1 : Game
 
     protected override void Initialize()
     {
-      
+      //
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
+        //Load Game Assets
+        //TODO 
+        // Get one game asset rendering on screen.
 
     }
 
     protected override void Update(GameTime gameTime)
     {
+        //Camera Control
+        //play sound()
+        //AI logic
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
+
+
+        //Rendering Code Only 
 
         base.Draw(gameTime);
     }
